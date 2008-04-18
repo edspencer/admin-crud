c601d93b15a82a561e57a1f27a2e57898138e344
Ed Spencer <edward@midlandswebdesign.com>
2008-04-18 12:49:45 +0100

    Added the actual generator code :)

--- admin_crud_generator.rb
+++ admin_crud_generator.rb
@@ -1,14 +1,13 @@
 class AdminCrudGenerator < Rails::Generator::Base
   def manifest 
     record do |m|
-      #do your stuff!
+      m.directory("app/controllers/admin")
+      m.directory("spec")
+      m.directory("spec/controllers")
+      m.directory("spec/controllers/admin")
       
-
-
-
-
-
-
+      m.file("controllers/crud_controller.rb", "app/controllers/admin/crud_controller.rb")
+      m.file("specs/crud_controller_spec.rb", "spec/admin/crud_controller_spec.rb")
       
       m.readme "../USAGE"
     end
