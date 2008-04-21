7e166364b01bc4031104ec7d95628e621b4c4d97
Ed Spencer <edward@midlandswebdesign.com>
2008-04-21 16:45:24 +0100

    Fixed generator problem

--- generators/admin_crud/admin_crud_generator.rb
+++ generators/admin_crud/admin_crud_generator.rb
@@ -7,7 +7,7 @@ class AdminCrudGenerator < Rails::Generator::Base
       m.directory("spec/controllers/admin")
       
       m.file("controllers/crud_controller.rb", "app/controllers/admin/crud_controller.rb")
-      m.file("specs/crud_controller_spec.rb", "spec/admin/crud_controller_spec.rb")
+      m.file("specs/crud_controller_spec.rb", "spec/controllers/admin/crud_controller_spec.rb")
       
       m.readme "../USAGE"
     end
